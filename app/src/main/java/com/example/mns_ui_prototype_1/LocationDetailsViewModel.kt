51f889a28b6a490f493ebb8343907b5099144d1a
lukas.grasmann.01633007 <e1633007@student.tuwien.ac.at>
2019-11-24 18:40:42 +0100

    Initial design and fragment navigation.

---
+++ app/src/main/java/com/example/mns_ui_prototype_1/LocationDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mns_ui_prototype_1
+
+import androidx.lifecycle.ViewModel
+
+class LocationDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/mns_ui_prototype_1/LocationListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mns_ui_prototype_1
+
+import androidx.lifecycle.ViewModel
+
+class LocationListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
