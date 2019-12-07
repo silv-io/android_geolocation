0902ce2553ca53db2eb3a0ff10df9ae3a0822140
Matthias Eder <matthiasbayern@github.com>
2019-12-07 02:24:45 +0100

    add not completed draft of a service for the celltower and wifi information

--- android_geolocation/src/main/java/at/tuwien/android_geolocation/service/MozillaLocationService.kt
+++ android_geolocation/src/main/java/at/tuwien/android_geolocation/service/MozillaLocationService.kt
@@ -1,12 +1,143 @@
 package at.tuwien.android_geolocation.service
 
+import android.Manifest
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+import android.os.Binder
 import android.os.IBinder
+import android.telephony.*
+import android.util.Log
+import androidx.core.content.ContextCompat
+import at.tuwien.android_geolocation.service.mls.CellTowerInfo
+import at.tuwien.android_geolocation.service.mls.WifiAccessPointInfo
 
 class MozillaLocationService : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    private val myBinder = MozillaLocationBinder()
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return myBinder
     }
+
+    inner class MozillaLocationBinder: Binder() {
+        fun getService(): MozillaLocationService {
+            return this@MozillaLocationService
+        }
+    }
+
+
+    fun getMLSInfo() {
+           if (ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_WIFI_STATE
+        ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted
+            Log.d(
+                "PERMISSION_DENIED",
+                "CHECK APP PERMISSIONS"
+            )
+
+        } else {
+            val telephonyManager: TelephonyManager =
+                this.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+            val wifiManager: WifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+            val cellTowers = getTowerInfo(telephonyManager.allCellInfo)
+            val wifiAccessPoints = getWifiAccessPointInfo(wifiManager.scanResults)
+
+               //do something with the information
+        }
+    }
+
+
+
+    private fun getTowerInfo(cellinfo: List<CellInfo>): MutableList<CellTowerInfo> {
+        val celltowers: MutableList<CellTowerInfo> = mutableListOf<CellTowerInfo>()
+        for (cell_info_entry in cellinfo) {
+            val newTower = CellTowerInfo()
+
+            newTower.radioType = when (cell_info_entry) {
+                is CellInfoWcdma -> "wcdma"
+                is CellInfoLte -> "lte"
+                is CellInfoGsm -> "gsm"
+                else -> null
+            }
+            newTower.mobileCountryCode = when (cell_info_entry) {
+                is CellInfoWcdma -> cell_info_entry.cellIdentity.mcc
+                is CellInfoLte -> cell_info_entry.cellIdentity.mcc
+                is CellInfoGsm -> cell_info_entry.cellIdentity.mcc
+                else -> null
+            }
+            newTower.mobileNetworkCode = when (cell_info_entry) {
+                is CellInfoWcdma -> cell_info_entry.cellIdentity.mnc
+                is CellInfoLte -> cell_info_entry.cellIdentity.mnc
+                is CellInfoGsm -> cell_info_entry.cellIdentity.mnc
+                else -> null
+            }
+            newTower.locationAreaCode = when (cell_info_entry) {
+                is CellInfoWcdma -> cell_info_entry.cellIdentity.lac
+                is CellInfoLte -> cell_info_entry.cellIdentity.tac
+                is CellInfoGsm -> cell_info_entry.cellIdentity.lac
+                else -> null
+            }
+            newTower.cellId = when (cell_info_entry) {
+                is CellInfoWcdma -> cell_info_entry.cellIdentity.cid
+                is CellInfoLte -> cell_info_entry.cellIdentity.ci
+                is CellInfoGsm -> cell_info_entry.cellIdentity.cid
+                else -> null
+            }
+            //Cell Tower age cannot be read by using SDK
+            //Cell Tower age out of scope
+            newTower.psc = when (cell_info_entry) {
+                is CellInfoWcdma -> cell_info_entry.cellIdentity.psc
+                is CellInfoLte -> cell_info_entry.cellIdentity.pci
+                is CellInfoGsm -> cell_info_entry.cellIdentity.psc
+                else -> null
+            }
+            newTower.signalStrength = when (cell_info_entry) {
+                is CellInfoWcdma -> cell_info_entry.cellSignalStrength.asuLevel
+                is CellInfoLte -> cell_info_entry.cellSignalStrength.asuLevel
+                is CellInfoGsm -> cell_info_entry.cellSignalStrength.asuLevel
+                else -> null
+            }
+            newTower.timingAdvance = when (cell_info_entry) {
+                is CellInfoLte -> cell_info_entry.cellSignalStrength.timingAdvance
+                else -> null
+            }
+            celltowers.add(newTower)
+        }
+        return celltowers
+    }
+
+    private fun getWifiAccessPointInfo(wifiInfo: List<ScanResult>): MutableList<WifiAccessPointInfo>{
+        val wifiAccessPoints: MutableList<WifiAccessPointInfo> = mutableListOf<WifiAccessPointInfo>()
+        for (wifi_info_entry in wifiInfo){
+            if(!wifi_info_entry.SSID.contains("_nomap")) {
+                val newWifiAccessPoint = WifiAccessPointInfo()
+                newWifiAccessPoint.macAddress = wifi_info_entry.BSSID
+                newWifiAccessPoint.age = wifi_info_entry.timestamp
+                newWifiAccessPoint.channel = wifi_info_entry.channelWidth
+                newWifiAccessPoint.frequency = wifi_info_entry.frequency
+                newWifiAccessPoint.signalStrength = wifi_info_entry.level
+                // Noise Level ist not available when using SDK
+                // Detection of noise level would require low-level NDK operations
+                // Noise Level out of scope
+                // newWifiAccessPoint.signalToNoiseRatio
+                newWifiAccessPoint.ssid = wifi_info_entry.SSID
+                wifiAccessPoints.add(newWifiAccessPoint)
+            }
+        }
+        return wifiAccessPoints
+    }
+
+
+
+
 }
--- android_geolocation/src/main/java/at/tuwien/android_geolocation/view/ui/location/LocationActivity.kt
+++ android_geolocation/src/main/java/at/tuwien/android_geolocation/view/ui/location/LocationActivity.kt
@@ -1,13 +1,33 @@
 package at.tuwien.android_geolocation.view.ui.location
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import at.tuwien.android_geolocation.service.MozillaLocationService
 import com.tuwien.geolocation_android.R
 
 class LocationActivity : AppCompatActivity() {
 
+    private var locationService: MozillaLocationService?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
+
+        val connection: ServiceConnection
+        connection = object : ServiceConnection {
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                locationService=null
+            }
+            override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
+                locationService=(service as MozillaLocationService.MozillaLocationBinder).getService()
+            }
+        }
+        locationService?.bindService(Intent(this, MozillaLocationService::class.java), connection, Context.BIND_AUTO_CREATE)
     }
+
 }
