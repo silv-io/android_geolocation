d5e56a44b240df300c5bcadd4fe10e9cda34ea64
Silvio <silvio.vasiljevic@gmail.com>
2019-12-10 23:45:39 +0100

    Add comment regarding mls api

--- android_geolocation/src/main/java/at/tuwien/android_geolocation/service/model/MLSAPI.kt
+++ android_geolocation/src/main/java/at/tuwien/android_geolocation/service/model/MLSAPI.kt
@@ -6,6 +6,13 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
+/*
+ * Our solution for having the API-key in a secure place would be to have the API-call below on a
+ * dedicated server which serves as a proxy. Because we don't have the infrastructure to do so,
+ * we call it here directly, because the response from the proxy would still be the same as the
+ * real API call. The main difference would be that we would need some sort of credential-system
+ * to access the proxy resource, which could e.g. be done by having account management.
+ */
 interface MLSAPI {
     @POST("geolocate?key=test")
     fun getMLSLocation(@Body mlsRequest: MLSRequest?): Call<MLSResponse?>?
